529efaa9cbbc25ac88d3eb200b4cb9a48f37cc77
Johannes Erber <johannes.erber@ext.airbnb.com>
2022-01-21 09:53:15 +0100

    Break homepage into components and style menu items

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,16 @@
+import "./menu-item.styles.scss";
+
+export const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`menu-item ${size}`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOW NOW</span>
+    </div>
+  </div>
+);
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,9 @@
+import { Directory } from "../../components/directory/directory.component";
+import { MenuItem } from "../../components/menu-item/menu-item.component";
+import "./homepage.styles.scss";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
