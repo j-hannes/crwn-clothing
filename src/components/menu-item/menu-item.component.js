216e013f4197307b4d55dcf60d2c650caabc95ee
Johannes Erber <johannes.erber@ext.airbnb.com>
2022-01-21 19:58:00 +0100

    [WIP] Add router

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,16 +1,19 @@
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOW NOW</span>
+export const MenuItem = withRouter(
+  ({ title, history, imageUrl, size, linkUrl }) => (
+    <div className={`menu-item ${size}`} onClick={() => history.push(linkUrl)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOW NOW</span>
+      </div>
     </div>
-  </div>
+  )
 );
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,5 +1,4 @@
 import { Directory } from "../../components/directory/directory.component";
-import { MenuItem } from "../../components/menu-item/menu-item.component";
 import "./homepage.styles.scss";
 
 export const HomePage = () => (
